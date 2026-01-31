245b6bd6f33513bce7a74b373b92e14fdfafd329
mnrmnr23 <nervosso5@gmail.com>
2026-01-31 13:49:08 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { MessageCircle, FileText, CreditCard } from 'lucide-react';
 
 function App() {
   // Substitua pelo número real do WhatsApp
-  const whatsappNumber = "5511954784014"; // Formato: código do país + DDD + número
+  const whatsappNumber = "5511960396670"; // Formato: código do país + DDD + número
 
   const handleWhatsAppRedirect = (message: string) => {
     const encodedMessage = encodeURIComponent(message);
